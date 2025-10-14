c5312d2ca055c6f73cef5ce06c34d46997849fa7
paweldol2003 <paweldolak2003@gmail.com>
2025-10-14 11:29:49 +0200

    Pathfinding functions setup

--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PathManager : MonoBehaviour
 {
 
     private GameManager gm;
     private Tile[,] grid;
-
+    public enum PathfindingAlgorithm { AStar, ACO, ABC }
+    [Header("Pathfinding")]
+    public PathfindingAlgorithm algorithm = PathfindingAlgorithm.ACO;
     public void Init(GameManager gm)
     {
         this.gm = gm;
@@ -19,7 +22,7 @@ public class PathManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetRandomPath()
@@ -48,12 +51,34 @@ public class PathManager : MonoBehaviour
         while (endTile == startTile) // nie ten sam
             endTile = walkable[UnityEngine.Random.Range(0, walkable.Count)];
 
-
+        FindPath(startTile, endTile);
 
         startTile.color = new Color32(255, 0, 0, 1);
         endTile.color = new Color32(255, 0, 0, 1);
         gm.gridManager.RefreshAll();
 
         Debug.Log($"Random start: ({startTile.x},{startTile.y}) | end: ({endTile.x},{endTile.y})");
+    }
+
+    public List<Tile> FindPath(Tile startTile, Tile endTile)
+    {
+        switch (algorithm)
+        {
+            case PathfindingAlgorithm.AStar:
+                return AStar(startTile, endTile);
+            // case PathfindingAlgorithm.ACO: // TODO
+            // case PathfindingAlgorithm.ABC: // TODO
+            default:
+                return ACO(startTile, endTile);
+        }
+    }
+    List<Tile> AStar(Tile startTile, Tile endTile)
+    {
+        return null;
+    }
+
+    List<Tile> ACO(Tile startTile, Tile endTile)
+    {
+        return null;
     }
 }
